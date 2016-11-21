b1c26bbc9b99a551b87f96aec8665fdebe8158c7
agnieszkablu <agnieszka.gorz@outlook.com>
2016-11-21 20:05:59 +0100

    userFill function- adds inputs, deletes prefilled words, adds class editable to spans, todo: validation of user inputs, some gaps arent prefilled-debug

--- src/js/script.js
+++ src/js/script.js
@@ -4,55 +4,59 @@
 	var tempNoun = document.querySelectorAll('.noun'),
 		tempVerb = document.querySelectorAll('.verb'),
 		tempAdj = document.querySelectorAll('.adj'),
-		nouns = ['mountains', 'ocean'],
-		verbs = ['live', 'supplies', 'flows'],
-		adjectives = ['small', 'large'],
+		nouns = ['mountains', 'ocean', 'lake', 'sun', 'shadow'],
+		verbs = ['live', 'supplies', 'flows', 'dies'],
+		adjectives = ['small', 'large', 'huge', 'little', 'tiny', 'enormous'],
 		preFill = document.getElementById('preFill'),
 		userInput = document.getElementById('userInput'),
 		//declare helper functions
 		//random number
 		randomise = function (numMax) {
 			return (Math.floor(Math.random() * numMax));
+		},
+		//function to iterate throught the nodelist
+		wordNodes = function (tempWord, arr, num) {
+			var item,
+				randNum = randomise(num),
+				splicedElem;
+				//using for.. of to iterate over selected nodelist and replace words
+			for (item of tempWord) {
+					//remove class editable
+				item.classList.remove('editable');
+				if (arr.length > 1) {
+					//splicedElem = arr.splice(randNum, 1);
+					item.innerHTML = arr.splice(randNum, 1);
+				} else if (arr.length === 1) {
+					item.innerHTML = arr.splice(0, 1);
+				} else {
+					item.innerHTML = 'Runned out of words';
+				}
+			}
+		},
+		//function adds inputs to each span (blank space)
+		addInputs = function (tempWord) {
+			var item,
+				input;
+			for (item of tempWord) {
+					//clear prefilled blanks
+				item.innerHTML = '';
+				input = document.createElement('input');
+				item.classList.add('editable');
+				item.appendChild(input);
+			}
 		};
-			//function to iterate throught the nodelist
-//		wordNodes = function (tempWord, word, num) {
-//			var item,
-//					randNum = randomise(num);
-//			for (item of tempWord) {
-//				item.innerHTML = word[randNum];
-//					console.log(randNum);
-//				console.log(word[randNum]);
-//			}
-//		};
 	//declare main functions
 	//Automatically fill blanks with predefined words
 	function fill() {
-		//using for.. of to iterate over selected nodelist
-			
-//		wordNodes(tempNoun, nouns, 2);
-			var item,
-					splicedElem;
-			for (item of tempNoun) {
-				
-				if (nouns.length > 1){
-					splicedElem = nouns.splice(randomise(2), 1);
-					item.innerHTML = splicedElem;
-				}else if(nouns.length == 1){
-						item.innerHTML = nouns.splice(0,1);
-				}else{
-						item.innerHTML = 'Runned out of words';
-				}
-			}
-			for (item of tempVerb) {
-				item.innerHTML = verbs[randomise(3)];
-			}
-			for (item of tempAdj) {
-				item.innerHTML = adjectives[randomise(2)];
-			}
+		wordNodes(tempNoun, nouns, 5);
+		wordNodes(tempVerb, verbs, 4);
+		wordNodes(tempAdj, adjectives, 6);
 	}
 	//Let users fill the gaps
 	function userFill() {
-		
+		addInputs(tempNoun);
+		addInputs(tempVerb);
+		addInputs(tempAdj);
 	}
 	
 	//attach eventListeners with specific functions to buttons 
