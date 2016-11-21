16f69a9c29a31c7910aa1bc87971a23e7b5a3f85
agnieszkablu <agnieszka.gorz@outlook.com>
2016-11-21 19:07:29 +0100

    experimenting with point 5 from objective list - working nouns

--- src/js/script.js
+++ src/js/script.js
@@ -4,51 +4,56 @@
 	var tempNoun = document.querySelectorAll('.noun'),
 		tempVerb = document.querySelectorAll('.verb'),
 		tempAdj = document.querySelectorAll('.adj'),
-		words = {
-			nouns : ['mountains', 'ocean'],
-			verbs : ['live', 'supplies', 'flows'],
-			adjectives : ['small', 'large']
-		},
+		nouns = ['mountains', 'ocean'],
+		verbs = ['live', 'supplies', 'flows'],
+		adjectives = ['small', 'large'],
 		preFill = document.getElementById('preFill'),
 		userInput = document.getElementById('userInput'),
 		//declare helper functions
 		//random number
 		randomise = function (numMax) {
-			var value;
-			value = Math.floor(Math.random() * numMax);
-			return value;
+			return (Math.floor(Math.random() * numMax));
 		};
-		//wordNodes = function (tempWord, num) {
+			//function to iterate throught the nodelist
+//		wordNodes = function (tempWord, word, num) {
 //			var item,
-//				randomNum = randomise(num);
-//			for ( item of tempWord ) {
-//				
-//				item.innerHTML = words.tempWord[randomNum];
+//					randNum = randomise(num);
+//			for (item of tempWord) {
+//				item.innerHTML = word[randNum];
+//					console.log(randNum);
+//				console.log(word[randNum]);
 //			}
-		//};
+//		};
 	//declare main functions
 	//Automatically fill blanks with predefined words
-	var fill = function () {
+	function fill() {
 		//using for.. of to iterate over selected nodelist
-		var item,
-			randomNum;
-		for ( item of tempNoun ) {
-			randomNum = randomise(2);
-			item.innerHTML = words.nouns[randomNum];
-		}
-		for ( item of tempVerb ) {
-			randomNum = randomise(3);
-			item.innerHTML = words.verbs[randomNum];
-		}
-		for ( item of tempAdj ) {
-			randomNum = randomise(2);
-			item.innerHTML = words.adjectives[randomNum];
-		}
-	};
+			
+//		wordNodes(tempNoun, nouns, 2);
+			var item,
+					splicedElem;
+			for (item of tempNoun) {
+				
+				if (nouns.length > 1){
+					splicedElem = nouns.splice(randomise(2), 1);
+					item.innerHTML = splicedElem;
+				}else if(nouns.length == 1){
+						item.innerHTML = nouns.splice(0,1);
+				}else{
+						item.innerHTML = 'Runned out of words';
+				}
+			}
+			for (item of tempVerb) {
+				item.innerHTML = verbs[randomise(3)];
+			}
+			for (item of tempAdj) {
+				item.innerHTML = adjectives[randomise(2)];
+			}
+	}
 	//Let users fill the gaps
-	var userFill = function () {
+	function userFill() {
 		
-	};
+	}
 	
 	//attach eventListeners with specific functions to buttons 
 	preFill.addEventListener('click', fill);
