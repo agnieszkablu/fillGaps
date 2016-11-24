463248f22303541c5b651b1fb76c397bc42ef8bb
agnieszkablu <agnieszka.gorz@outlook.com>
2016-11-24 19:41:52 +0100

    added function for user input - first simple validation by checking for letters, also added output element for future validation outputs

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,7 @@
 		adjectives = ['small', 'large', 'huge', 'little', 'tiny', 'enormous'],
 		preFill = document.getElementById('preFill'),
 		userInput = document.getElementById('userInput'),
+		validationOutput = document.getElementById('validationOutput'),
 		//declare helper functions
 		//random number
 		randomise = function (numMax) {
@@ -18,35 +19,55 @@
 		wordNodes = function (tempWord, arr) {
 			var i,
 				randNum;
-				//splicedElem;
-				//using for.. of to iterate over selected nodelist and replace words
+				//using for..  to iterate over selected nodelist and replace words
 			for (i = 0; i < tempWord.length; i++) {
 					//remove class editable when user change from their own input to prefill
 				tempWord[i].classList.remove('editable');
 				//console.log(item);
 				randNum = randomise(arr.length);
 				if (arr.length > 1) {
-					//splicedElem = arr.splice(randNum, 1);
-					tempWord[i].innerHTML = arr.splice(randNum, 1);
-					console.log(arr.length);
+					tempWord[i].innerText = arr.splice(randNum, 1);
 				} else if (arr.length === 1) {
-					tempWord[i].innerHTML = arr.splice(0, 1);
+					tempWord[i].innerText = arr.splice(0, 1);
 				} else {
-					tempWord[i].innerHTML = 'Runned out of words';
+					tempWord[i].innerText = 'Runned out of words';
 				}
 			}
 		},
+		inputValid = function () {
+			var letters = /^[a-zA-Z]+$/;
+			if ( this.value.match(letters) ){
+				this.parentElement.innerText = this.value;
+//				validationOutput.classList.add('success');
+//				validationOutput.childNodes[0].innerText = 'Clever story ;)';
+				console.log('letters');
+				//inputVal(this.value);
+			} else {
+				this.style.borderBottomColor = 'red';
+//				validationOutput.classList.add('error');
+//				validationOutput.childNodes[0].innerText = 'Please use only letters, and single words';
+				console.log('not letters');
+				return false;
+			}
+		},
+//		inputVal = function (val) {
+//			
+//		},
 		//function adds inputs to each span (blank space)
 		addInputs = function (tempWord) {
 			var item,
 				input;
 			//using for.. of to iterate over selected nodelist and glue inputs to them
 			for (item of tempWord) {
-					//clear prefilled blanks
-				item.innerHTML = '';
-				input = document.createElement('input');
+					//clear prefilled blanks and add class editable to each one
+				item.innerText = '';
 				item.classList.add('editable');
+				//create input and append it to each item
+				input = document.createElement('input');
 				item.appendChild(input);
+				//add events to each created inputs
+				//input.addEventListener('click', inputVal);
+				input.addEventListener('blur', inputValid);
 			}
 		};
 	//declare main functions
@@ -62,8 +83,7 @@
 		addInputs(tempVerb);
 		addInputs(tempAdj);
 	}
-	
-	//attach eventListeners with specific functions to buttons 
+	//attach eventListeners with specific functions to elements 
 	preFill.addEventListener('click', fill);
 	userInput.addEventListener('click', userFill);
 }());
